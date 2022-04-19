0a8b1d04ee7d522414a75e83da78ee34cfb6ddf8
Shafayathub <shafayat.ms@gamilcom>
2022-04-19 17:54:47 +0600

    deposit fixed

---
+++ js/banking.js
@@ -0,0 +1,13 @@
+// Handle deposit button event
+document.getElementById('deposit-button').addEventListener('click', function () {
+    // get the amount deposited
+    const depositInput = document.getElementById('deposit-input');
+    const depositAmount = depositInput.value;
+    console.log(depositAmount);
+
+    const depositTotal = document.getElementById('deposit-total');
+    depositTotal.innerText = depositAmount;
+
+    // Clear the deposit input field
+    depositInput.value = '';
+})
